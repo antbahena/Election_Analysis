282598e1e75c74aee1130ad3e51533f47d528cc1
antbahena <antbahena@gmail.com>
2022-12-06 13:51:02 -0800

    Adding the election audit code.

---
+++ Python_practice.py
@@ -0,0 +1,8 @@
+counties_dict = {}
+counties_dict["Arapahoe"] = 422829
+
+counties_dict["Denver"] = 463353
+counties_dict["Jefferson"] = 432438
+print(counties_dict)
+print(len(counties_dict))
+print(counties_dict.items())
